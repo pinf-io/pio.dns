932deef88824ade37c6c486b7f9290c1ea898d78
Christoph Dorn <christoph@christophdorn.com>
2014-05-26 16:36:39 -0700

    digital ocean DNS adapter

--- adapters/digitalocean.js
+++ adapters/digitalocean.js
@@ -1,80 +1,107 @@
-/*
 
-	self.dns = {
-		ensure: function(records) {
-			return self._ready.promise.then(function() {
-				return self._api.domainGetAll().then(function(domains) {
-					var recordsByDomainId = {};
+const ASSERT = require("assert");
+const CRYPTO = require("crypto");
+const Q = require("q");
+const DIGIO = require("digitalocean-api");
+const DEEPMERGE = require("deepmerge");
+const WAITFOR = require("waitfor");
+
+// @see https://developers.digitalocean.com/
+// @see https://github.com/enzy/digitalocean-api
+
+
+var adapter = exports.adapter = function(settings) {
+
+	var self = this;
+
+	self._settings = settings;
+
+	ASSERT.equal(typeof self._settings.clientId, "string");
+	ASSERT.equal(typeof self._settings.apiKey, "string");
+
+	var api = new DIGIO(self._settings.clientId, self._settings.apiKey);
+	self._api = {};
+	for (var name in api) {
+		if (typeof api[name] === "function") {
+			(function inject(name) {
+				self._api[name] = function() {
+					var args = Array.prototype.slice.call(arguments, 0);
+					return Q.nbind(api[name], api).apply(api, args);
+				}
+			})(name);
+		}
+	}
+}
+
+adapter.prototype.ensure = function(records) {
+	var self = this;
+	return self._api.domainGetAll().then(function(domains) {
+		var recordsByDomainId = {};
+		var done = Q.resolve();
+		records.forEach(function(record) {
+			var domainId = null;
+			domains.forEach(function(domain) {
+				if (domainId) return;
+				if (record.domain === domain.name) {
+					domainId = domain.id;
+				}
+				if (record.type === "CNAME") {
+					record.data += ".";
+				}
+			});
+			if (!domainId) {
+				throw new Error("Unable to find domain `" + record.domain + "`. Looks like top-level DNS record for the domain is not provisioned!");
+			}
+			if (!recordsByDomainId[domainId]) {
+				recordsByDomainId[domainId] = [];
+			}
+			recordsByDomainId[domainId].push(record);
+		});
+		// TODO: Parallelize all these calls.
+		Object.keys(recordsByDomainId).forEach(function(domainId) {
+			done = Q.when(done, function() {
+				return self._api.domainRecordGetAll(domainId).then(function(existingRecords) {
 					var done = Q.resolve();
-					records.forEach(function(record) {
-						var domainId = null;
-						domains.forEach(function(domain) {
-							if (domainId) return;
+					recordsByDomainId[domainId].filter(function(record) {
+						return (existingRecords.filter(function(existingRecord) {
 							if (
-								record.domain === domain.name ||
-								record.domain.substring(record.domain.length-domain.name.length-1) === "." + domain.name
+								record.type === existingRecord.record_type &&
+								record.name === existingRecord.name
 							) {
-								domainId = domain.id;
-								record.name = record.name.replace(new RegExp("\\." + domain.name + "$"), "");
-							}
-						});
-						if (!domainId) {
-							throw new Error("Unable to determine `domainId` for domain `" + record.domain + "`. Looks like top-level DNS record for the domain is not provisioned!");
-						}
-						if (!recordsByDomainId[domainId]) {
-							recordsByDomainId[domainId] = [];
-						}
-						recordsByDomainId[domainId].push(record);
-					});
-					// TODO: Parallelize all these calls.
-					Object.keys(recordsByDomainId).forEach(function(domainId) {
+								if (record.data === existingRecord.data) {
+									return true;
+								}
+								record._needsUpdate = existingRecord.id;
+							};
+							return false;
+						}).length === 0);
+					}).forEach(function(createRecord) {
 						done = Q.when(done, function() {
-							return self._api.domainRecordGetAll(domainId).then(function(existingRecords) {
-								var done = Q.resolve();
-								recordsByDomainId[domainId].filter(function(record) {
-									return (existingRecords.filter(function(existingRecord) {
-										if (
-											record.type === existingRecord.record_type &&
-											record.name === existingRecord.name
-										) {
-											if (record.data === existingRecord.data + ".") {
-												return true;
-											}
-											record._needsUpdate = existingRecord.id;
-										};
-										return false;
-									}).length === 0);
-								}).forEach(function(createRecord) {
-									done = Q.when(done, function() {
-										if (createRecord._needsUpdate) {
-											console.log(("Updating DNS record: " + JSON.stringify(createRecord)).magenta);
-											return self._api.domainRecordEdit(
-												domainId,
-												createRecord._needsUpdate,
-												createRecord.type,
-												createRecord.data + ".",
-												{}
-											);
-										}
-										console.log(("Creating DNS record: " + JSON.stringify(createRecord)).magenta);
-										return self._api.domainRecordNew(
-											domainId,
-											createRecord.type,
-											createRecord.data + ".",
-											{
-												name: createRecord.name
-											}
-										);
-									});
-								});
-								return done;
-							});
+							if (createRecord._needsUpdate) {
+								console.log(("Updating DNS record: " + JSON.stringify(createRecord)).magenta);
+								return self._api.domainRecordEdit(
+									domainId,
+									createRecord._needsUpdate,
+									createRecord.type,
+									createRecord.data,
+									{}
+								);
+							}
+							console.log(("Creating DNS record: " + JSON.stringify(createRecord)).magenta);
+							return self._api.domainRecordNew(
+								domainId,
+								createRecord.type,
+								createRecord.data,
+								{
+									name: createRecord.name
+								}
+							);
 						});
 					});
 					return done;
 				});
 			});
-		}
-	};
-
-*/
+		});
+		return done;
+	});
+}
